ebd2135a7f7dba450e33d9878cba6bda40280cb0
JovanJJ <jovanjj99@gmail.com>
2026-05-14 20:00:39 +0200

    Fix deploy error

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -11,7 +11,7 @@ const pool = new Pool({
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2023-10-16", // Use the latest API version
+    apiVersion: "2026-04-22.dahlia", // Use the latest API version
 });
 
 export async function POST(req: Request) {
